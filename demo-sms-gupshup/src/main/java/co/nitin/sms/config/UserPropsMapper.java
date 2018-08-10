d81f479e9198b3395f586e93f6933d3344679752
Nitin Sharma <nitinsharmares@gmail.com>
2018-08-10 13:32:14 +0530

    Adding properties file and its mapper.

---
+++ demo-sms-gupshup/src/main/java/co/nitin/sms/config/UserPropsMapper.java
@@ -0,0 +1,34 @@
+package co.nitin.sms.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Mapping for `smsprovider.properties` file. Will help in fetching whole properties file as an object.
+ * 
+ * @author Nitin Sharma
+ *
+ */
+@PropertySource("classpath:smsprovider.properties")
+@ConfigurationProperties(prefix = "user")
+@Component
+public class UserPropsMapper {
+
+	private long id;
+	private String password;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+}
