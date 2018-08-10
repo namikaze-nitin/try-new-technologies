e69739f3a632f93731b296d93b60656f33fcb3c2
Nitin Sharma <nitinsharmares@gmail.com>
2018-08-10 13:15:41 +0530

    Created Project for testing SMS Service Provider:GupShup

---
+++ demo-sms-gupshup/src/main/java/co/nitin/sms/DemoSmsGupshupApplication.java
@@ -0,0 +1,12 @@
+package co.nitin.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSmsGupshupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSmsGupshupApplication.class, args);
+	}
+}
