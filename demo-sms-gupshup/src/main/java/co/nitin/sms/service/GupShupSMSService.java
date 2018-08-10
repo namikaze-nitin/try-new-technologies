b9b2451a978a07ae939a6f8cf447a72cee721fb5
Nitin Sharma <nitinsharmares@gmail.com>
2018-08-10 16:20:12 +0530

    Added test cases to check phone number formatting and message formatting.
    Removed brackets from password : we were getting password in format
    ""pass"">dont need double quotes.

--- demo-sms-gupshup/src/main/java/co/nitin/sms/service/GupShupSMSService.java
+++ demo-sms-gupshup/src/main/java/co/nitin/sms/service/GupShupSMSService.java
@@ -12,7 +12,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import co.nitin.sms.config.UserPropsMapper;
@@ -20,7 +19,7 @@ import co.nitin.sms.config.UserPropsMapper;
 @Service
 public class GupShupSMSService {
 
-	Logger log =  LoggerFactory.getLogger(GupShupSMSService.class);
+	private Logger log =  LoggerFactory.getLogger(GupShupSMSService.class);
 	private UserPropsMapper propsMapper;
 	
 	@Autowired
@@ -30,7 +29,7 @@ public class GupShupSMSService {
 
 	public String sendSimpleSMS(String message, List<Long> phoneNos, String[] apiVar) throws IOException {
 		
-		log.info("[sendSMS]");
+		log.info("[sendSimpleSMS]");
 		
 		String messageBody = this.messageBodyFormatter(message, phoneNos, apiVar);
 		
@@ -56,31 +55,34 @@ public class GupShupSMSService {
 		return buffer.toString();
 	}
 	
-	private String messageBodyFormatter(String message, List<Long> phoneNos, String[] apiVar) throws UnsupportedEncodingException {
-
-		String data = "";
-		data += "method=sendMessage";
-		data += "&userid=" 		+ this.propsMapper.getId(); // your loginId
-		data += "&password=" 	+ URLEncoder.encode(this.propsMapper.getPassword(), "UTF-8"); // your password
-		data += "&msg=" 		+ URLEncoder.encode(message , "UTF-8");
-		data += "&send_to=" 	+ URLEncoder.encode( this.recepientNumberFormatter(phoneNos) , "UTF-8"); // a valid 10 digit phone no.
-		data += "&v=" 			+ apiVar[0];
-		data += "&format=" 		+ apiVar[1];
-		data += "&msg_type=" 	+ apiVar[2]; // Can by "FLASH" or "UNICODE_TEXT" or “BINARY”
-		data += "&auth_scheme=" + apiVar[3];
+	protected String messageBodyFormatter(String message, List<Long> phoneNos, String[] apiVar) throws UnsupportedEncodingException {
+	
+		log.info("[messageBodyFormatter]");
 		
-		return data;
+		StringBuilder msg= new StringBuilder();
+		msg.append("method=sendMessage");
+		msg.append("&userid=" 		+ this.propsMapper.getId()); // your loginId
+		msg.append("&password=" 	+ URLEncoder.encode(this.propsMapper.getPassword(), "UTF-8")); // your password
+		msg.append("&msg=" 			+ URLEncoder.encode(message , "UTF-8"));
+		msg.append("&send_to=" 		+ URLEncoder.encode( this.recepientNumberFormatter(phoneNos) , "UTF-8")); // a valid 10 digit phone no.
+		msg.append("&v=" 			+ apiVar[0]);
+		msg.append("&format=" 		+ apiVar[1]);
+		msg.append("&msg_type=" 	+ apiVar[2]); // Can by "FLASH" or "UNICODE_TEXT" or “BINARY”
+		msg.append("&auth_scheme=" 	+ apiVar[3]);
+		
+		return msg.toString();
 	}
 	
-	private String recepientNumberFormatter(List<Long> phoneNo) {
+	protected String recepientNumberFormatter(List<Long> phoneNo) {
+		
+		log.info("[recepientNumberFormatter]");
 		
 		int size = phoneNo.size();
 		StringBuilder builder = new StringBuilder();
 		
 		for(int i=0; i<size; i++) 
 			builder.append(phoneNo.get(i)).append(",");
-
-		builder.deleteCharAt(builder.capacity());
+		builder.deleteCharAt(builder.length()-1);		
 		
 		return builder.toString();
 	}
---
+++ demo-sms-gupshup/src/test/java/co/nitin/sms/service/TestSimpleSMS.java
@@ -0,0 +1,71 @@
+package co.nitin.sms.service;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import co.nitin.sms.config.UserPropsMapper;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSimpleSMS {
+
+	@Autowired UserPropsMapper mapper;
+	static Logger log = LoggerFactory.getLogger("[TestSimpleSMS]");
+	
+	@Test
+	public void numberFormatterTest() {
+		
+		log.info("[simpleSMSTest]");
+		List<Long> number = new ArrayList<Long>();
+		number.add(9870453104L);
+		number.add(8209163590L);
+		
+		GupShupSMSService service = new GupShupSMSService(mapper);
+		Assert.assertEquals(service.recepientNumberFormatter(number),"9870453104,8209163590");
+	}
+	
+	@Test
+	public void messageFormatterTest() throws UnsupportedEncodingException {
+		
+		log.info("[messageFormatterTest]");
+		
+		GupShupSMSService service = new GupShupSMSService(mapper);
+		
+		Assert.assertEquals( URLEncoder.encode(this.mapper.getPassword(), "UTF-8"), URLEncoder.encode("java@neurosys3", "UTF-8") );
+
+		List<Long> number = new ArrayList<Long>();
+		number.add(9870453104L);
+		number.add(8209163590L);
+		Assert.assertEquals(service.recepientNumberFormatter(number),"9870453104,8209163590");
+
+		System.out.println(mapper);
+		String[] var = {"1.1","json","TEXT","PLAIN"};
+		String msgBody = service.messageBodyFormatter("Hello!!!", number, var);
+//		System.out.println(service.messageBodyFormatter("Hello!!!", number, var));
+		
+		String data = "";
+		data += "method=sendMessage";
+		data += "&userid=2000181134"; // your loginId
+		data += "&password=" + URLEncoder.encode("java@neurosys3", "UTF-8"); // your password
+		data += "&msg=" + URLEncoder.encode("Hello!!!" , "UTF-8");
+		data += "&send_to=" + URLEncoder.encode(service.recepientNumberFormatter(number), "UTF-8"); // a valid 10 digit phone no.
+		data += "&v=1.1" ;
+		data += "&format=json";
+		data += "&msg_type=TEXT"; // Can by "FLASH" or "UNICODE_TEXT" or “BINARY”
+		data += "&auth_scheme=PLAIN";
+		
+		Assert.assertEquals(msgBody, data);
+	}
+	
+}
